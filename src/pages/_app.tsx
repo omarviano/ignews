f1c1390cdf58cf4f46f5aa8d712482afa71fffba
Marviano <marcosbts71@gmail.com>
2021-10-03 11:34:33 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AppProps } from "next/app";
+
+import { Header } from "../components/Header";
+
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default MyApp;
